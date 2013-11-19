703077eb67412bf1f4ba4bdbe1f8ca69ed9e8650
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-19 15:33:38 -0800

    fixed bug with SFM module that had methods incorrectly defined

--- lib/sfm.rb
+++ lib/sfm.rb
@@ -1,12 +1,12 @@
 module SFM
 
-	def initialize
+  def self.init
 		@client = Databasedotcom::Client.new client_id: ENV['DATABASEDOTCOM_CLIENT_ID'], client_secret: ENV['DATABASEDOTCOM_CLIENT_SECRET']
 		@client.authenticate username: ENV['SALESFORCE_USERNAME'], password: ENV['SALESFORCE_PASSWORD']
 		@client.sobject_module = "SFM"
-	end
+  end
 
-	def retrieve_model(model)
+	def self.retrieve_model(model)
 		@client.materialize(model)
 	end
 	
