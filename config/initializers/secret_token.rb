9fbe0b8d44cd01dfdb1e883975783c58206c690c
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-11-18 21:52:48 -0800

    Generate new rails application and declare intent in readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaundRi::Application.config.secret_key_base = '56a070a58a5ea166838343992e3e36185f5e9480c90eef4e6157ce952d06e6eb029bf4a4712ff2c0b78bbc05256c5b1859c95955d14c525bbec209c6aa9aaccd'
