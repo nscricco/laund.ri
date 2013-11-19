c47f16eaa79a16fb0e4373a0b4150f5c42d90cb3
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-19 01:38:43 -0800

    fixed typo: driver not user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by_username(params[:session][:username])
-    if @user && @user.authenticate(params[:session][:password])
-      log_in(@user)
+    @driver = Driver.find_by_username(params[:session][:username])
+    if @driver && @driver.authenticate(params[:session][:password])
+      log_in(@driver)
     end
   end
 
