f4cc5da3bab0a26c06601a67d553ef5126cf5819
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-19 22:09:15 -0800

    Renamed env client_id and client_secret to accurately reflect REST API

--- lib/sfm.rb
+++ lib/sfm.rb
@@ -1,7 +1,7 @@
 module SFM
 
   def self.init
-		@client = Databasedotcom::Client.new client_id: ENV['DATABASEDOTCOM_CLIENT_ID'], client_secret: ENV['DATABASEDOTCOM_CLIENT_SECRET']
+		@client = Databasedotcom::Client.new client_id: ENV['REST_API_CLIENT_ID'], client_secret: ENV['REST_API_CLIENT_SECRET']
 		@client.authenticate username: ENV['SALESFORCE_USERNAME'], password: ENV['SALESFORCE_PASSWORD']
 		@client.sobject_module = "SFM"
   end
