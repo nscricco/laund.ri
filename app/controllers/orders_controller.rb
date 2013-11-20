7a46a0f3d0d66fb954676dd5cc408b08b0cd1758
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-20 14:05:39 -0800

    Index displays only orders ready for pickup on load

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
 
   def index
     @active_driver = ActiveDriver.new
-    @orders = Order.all
+    @orders = Order.ready_for_pickup
   end
 
   def show
