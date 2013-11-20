36943668c5539613f6c534ec20f02dddd52392de
kbsail <berry.kevinw@gmail.com>
2013-11-20 01:26:52 -0800

    created individual order layout

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ActiveRecord::Base
   has_many :orders
+
+
+  def customer_attributes
+    titles = ['Customer_id', 'Name', 'Street', 'City', 'State', 'Zip', 'Phone', 'Email', 'Preferences']
+    values = self.attributes.map {|key, value| value}
+    Hash[titles.zip(values)]
+  end
+
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,12 @@
 class Order < ActiveRecord::Base
   belongs_to :customer
-  # include OrderHelper
+
+  def order_attributes
+    titles = ['Order Id', 'Weight', 'Price', 'Number of Items', 'Type', 'Ready for Pickup?', 'En Route?', 'Arrived?', 'Order Completed?', 'Delivered', 'Customer ID']
+    values = self.attributes.map {|key, value| value}
+    Hash[titles.zip(values)]
+  end
+
 
 
 end
