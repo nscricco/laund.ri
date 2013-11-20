5847292a2f8b759d85e160ddbb07409dd31a1ea5
kbsail <berry.kevinw@gmail.com>
2013-11-19 20:00:21 -0800

    worked on drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,10 +1,12 @@
 class DriversController < ApplicationController
  def create
-    if @driver = Driver.find_by_email(params[:email])
+    if @driver = Driver.find_by(Email__c: params[:email])
       @driver.save(params)
       log_in(@driver)
-      puts "******************************************"
+      puts "#################"
       puts session
+      puts "#################"
+
     end
     redirect_to root_path
   end
