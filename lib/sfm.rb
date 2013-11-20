056f62596434f928bed9453db906058dbf78a814
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-19 18:30:20 -0800

    takes a single  sfo and creates an assoiated  ar entry

--- lib/sfm.rb
+++ lib/sfm.rb
@@ -9,5 +9,25 @@ module SFM
 	def self.retrieve_model(model)
 		@client.materialize(model)
 	end
+
+  def self.get_sfo_params(object)
+    params = {}
+    object.attributes.each do
+      |attribute| params[attribute[0]] = attribute[1]
+    end
+  end
+
+  def self.create_database_entry(table_string, params)
+    table_string = table_string.capitalize
+    table_name = Object.const_get(table_string)
+    table_attributes = SFM.get_attributes(table_name)
+    params = ActionController::Parameters.new(params)
+    params = params.permit(relevant_params)
+    table.new(params)
+  end
+
+  def self.get_attributes(table_name)
+    table_name.new.attributes.keys
+  end
 	
 end 
--- lib/tasks/salesforce_retrieve.rake
+++ lib/tasks/salesforce_retrieve.rake
@@ -4,5 +4,7 @@ namespace :jobs do
     SFM.init
 		orders = SFM.retrieve_model('Order__c')
 		customers = SFM.retrieve_model('Customer__c')
+    params = SFM.get_sfo_params(orders.first)
+    SFM.create_database_entry("order", params)
 	end
 end
