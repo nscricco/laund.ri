32b3884e3c86b412c755b0c439201d1ae804c864
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-20 16:39:21 -0800

    working finally

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,10 @@
 class OrdersController < ApplicationController
-  include Databasedotcom::Rails::Controller
+  include SFM
+
   def index
     @active_driver = ActiveDriver.new
     @orders = Order.ready_for_pickup
+    p @orders
   end
 
   def show
@@ -13,11 +15,21 @@ class OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id], include: :customer)
+    client = get_client
+    sf_id = @order.Id
+    order_sf = SFM.retrieve_model("Order__c").find(sf_id)
     if params[:confirming] == 'delivered'  && @order.delivered__c.nil?
       @order.touch(:delivered__c)
+      order_sf.update_attributes "delivered__c" => @order.delivered__c
     elsif params[:confirming] == 'en_route' && @order.en_route__c.nil?
       @order.touch(:en_route__c)
+      order_sf.update_attributes "delivered__c" => @order.delivered__c
     end
+  end
+
+  def get_client
+    SFM.init
+    SFM.client
   end
 
 end
--- lib/sfm.rb
+++ lib/sfm.rb
@@ -1,9 +1,12 @@
 module SFM
 
   def self.init
-		@client = Databasedotcom::Client.new client_id: ENV['DATABASEDOTCOM_CLIENT_ID'], client_secret: ENV['DATABASEDOTCOM_CLIENT_SECRET']
+		@client = Databasedotcom::Client.new client_id: ENV['REST_API_CLIENT_ID'], client_secret: ENV['REST_API_CLIENT_SECRET']
 		@client.authenticate username: ENV['SALESFORCE_USERNAME'], password: ENV['SALESFORCE_PASSWORD']
-		@client.sobject_module = "SFM"
+  end
+
+  def self.client
+    @client
   end
 
 	def self.retrieve_model(model)
